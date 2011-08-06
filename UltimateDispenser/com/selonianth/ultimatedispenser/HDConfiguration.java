b7cac06de0aabaaa443a2b79e9b6c5fb172644f8
Selonianth <lorienelf92@yahoo.com>
2011-08-05 20:42:15 -0400

    Created HDConfiguration Class and Corrected Errors in UltimateDispenser

---
+++ UltimateDispenser/com/selonianth/ultimatedispenser/HDConfiguration.java
@@ -0,0 +1,5 @@
+package com.selonianth.ultimatedispenser;
+
+public class HDConfiguration {
+
+}
--- UltimateDispenser/com/selonianth/ultimatedispenser/UltimateDispenser.java
+++ UltimateDispenser/com/selonianth/ultimatedispenser/UltimateDispenser.java
@@ -1,75 +1,75 @@
 package com.selonianth.ultimatedispenser;
 
-import java.security.Permissions;
+import java.io.File;
 import java.util.logging.Logger;
 
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.File;
+import com.nijiko.permissions.PermissionHandler;
+import com.nijikokun.bukkit.Permissions.Permissions;
 
 public class UltimateDispenser extends JavaPlugin {
 
-	//public SWConfiguration swConfig;
-	public static final Logger LOG = Logger.getLogger("Minecraft");
-	public static final String LOG_PREFIX = "[UltimateDispenser]";
+    // public SWConfiguration swConfig;
+    public static final Logger LOG = Logger.getLogger("Minecraft");
+    public static final String LOG_PREFIX = "[UltimateDispenser]";
+    private PermissionHandler permissionHandler;
+    @Override
+    public void onDisable() {
+        // TODO Auto-generated method stub
+        System.out.println("UltimateDispenser Disabled");
+    }
 
-	@Override
-	public void onDisable() {
-		// TODO Auto-generated method stub
-	    System.out.println("UltimateDispenser Disabled");	
-	}
+    private void writeConfig() {
+        // Save config
+        // if(!this.swConfig.write(this)) {
+        LOG.warning(LOG_PREFIX + "Failed to save configuration file (version " + this.getDescription().getVersion() + "); continuing anyway...");
+    }
 
-	private void writeConfig() {
-		//Save config
-	 //if(!this.swConfig.write(this)) {
-		LOG.warning(LOG_PREFIX + "Failed to save configuration file (version " +this.getDescription().getVersion()+ "); continuing anyway...");
-		}
+    @Override
+    public void onEnable() {
+        // TODO Auto-generated method stub
+        System.out.println("UltimateDispenser Enabled");
 
-	
-	@Override
-	public void onEnable() {
-		// TODO Auto-generated method stub
-		System.out.println("UltimateDispenser Enabled");
-	
-		// Initialize Permissions system
-		this.setupPermissions();}
-		
-	
-		private void setupPermissions() {
-			Plugin permissionsPlugin = this.getServer().getPluginManager().getPlugin("Permissions");
+        // Initialize Permissions system
+        this.setupPermissions();
+    }
 
-			//if(this.permissionHandler == null) {
-			if(permissionsPlugin != null) {
-			//this.permissionHandler = ((Permissions) permissionsPlugin).getHandler();
-			LOG.info(LOG_PREFIX + "Hooked into Permissions version " + permissionsPlugin.getDescription().getVersion());
-			} else {
-			LOG.info(LOG_PREFIX + "Permissions system not detected; allowing all commands");
-			}
-	//	}
+    private void setupPermissions() {
+        Plugin permissionsPlugin = this.getServer().getPluginManager().getPlugin("Permissions");
 
+        // if(this.permissionHandler == null) {
+        if (permissionsPlugin != null) {
+         this.permissionHandler = ((Permissions) permissionsPlugin).getHandler();
+            LOG.info(LOG_PREFIX + "Hooked into Permissions version " + permissionsPlugin.getDescription().getVersion());
+        } else {
+            LOG.info(LOG_PREFIX + "Permissions system not detected; allowing all commands");
+        }
+        // }
 
-		// Get a configuration
-		this.readConfig();
+        // Get a configuration
+        this.readConfig();
 
-		LOG.info(LOG_PREFIX + "Version " + this.getDescription().getVersion() + " enabled");
-		}
+        LOG.info(LOG_PREFIX + "Version " + this.getDescription().getVersion() + " enabled");
+    }
 
-		private void readConfig(){
-		//Set up configuration folder
-		this.getDataFolder().mkdirs();}
+    private void readConfig() {
+        // Set up configuration folder
+        this.getDataFolder().mkdirs();
+    }
 
-
-		// Read configuration file
-		//File configFile = new File(this.getDataFolder(), SWConfiguration.CONFIG_FILE_NAME);{
-		//this.swConfig = SWConfiguration.detectConfiguration(configFile);
-		//while(this.swConfig.getVersion() != SWConfiguration.LATEST_CONFIG_VERSION) {
-		//SWConfiguration upgraded = this.swConfig.upgrade();
-		//if(upgraded == null) {
-		//LOG.warning(LOG_PREFIX + "Unable to upgrade configuration from version " + this.swConfig.getVersion() + ".");
-		//LOG.warning(LOG_PREFIX + "You may experience some instability. Continuing anyway...");
-		//break;
-		//}
-		//this.swConfig = upgraded;}
-		}
+    // Read configuration file
+   // File configFile = new File(this.getDataFolder();{
+    // SWConfiguration.CONFIG_FILE_NAME);{
+    // this.swConfig = SWConfiguration.detectConfiguration(configFile);
+    //while(this.swConfig.getVersion() != SWConfiguration.LATEST_CONFIG_VERSION) {
+    //SWConfiguration upgraded = this.swConfig.upgrade();
+   // if(upgraded == null) {
+    //LOG.warning(LOG_PREFIX + "Unable to upgrade configuration from version "
+    // + this.swConfig.getVersion() + ".");
+    //LOG.warning(LOG_PREFIX +"You may experience some instability. Continuing anyway...");
+    //break;
+    //}
+    // this.swConfig = upgraded;}
 }
