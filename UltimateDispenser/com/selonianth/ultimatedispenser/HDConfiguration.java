36b7518850627821c592b02c2c690aac2c268aba
Selonianth <lorienelf92@yahoo.com>
2011-08-05 21:41:55 -0400

    Added Plugin.yml with udfill command and added content to HDConfiguration

--- UltimateDispenser/com/selonianth/ultimatedispenser/HDConfiguration.java
+++ UltimateDispenser/com/selonianth/ultimatedispenser/HDConfiguration.java
@@ -2,9 +2,13 @@ package com.selonianth.ultimatedispenser;
 
 import java.io.File;
 
+import org.bukkit.util.config.Configuration;
+
 public class HDConfiguration {
 	private File file;
+	private Configuration configuration;
 	
 	public HDConfiguration(File file){
     this.file = file;
+    this.configuration = new Configuration(file);
 }}
