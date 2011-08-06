9d2cc875a3152b5b193ad53961fe520e29368dd4
Selonianth <lorienelf92@yahoo.com>
2011-08-06 16:53:59 -0400

    Add code to UltimateDispenser.java

--- UltimateDispenser/com/selonianth/ultimatedispenser/UltimateDispenser.java
+++ UltimateDispenser/com/selonianth/ultimatedispenser/UltimateDispenser.java
@@ -3,8 +3,10 @@ package com.selonianth.ultimatedispenser;
 import java.io.File;
 import java.util.logging.Logger;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -16,8 +18,9 @@ public class UltimateDispenser extends JavaPlugin {
     public HDConfiguration hdConfig;
     public static final Logger LOG = Logger.getLogger("Minecraft");
     public static final String LOG_PREFIX = "[UltimateDispenser]";
-    private PermissionHandler permissionHandler;
-
+ // Permissions interface
+    public PermissionHandler permissionHandler;
+    public static final String PERMISSION_ROOT_NAME = "UltimateDispenser";
     @Override
     public void onDisable() {
         // TODO Auto-generated method stub
@@ -31,6 +34,9 @@ public class UltimateDispenser extends JavaPlugin {
         }
     }
 
+ // Command name
+    public static final String COMMAND_NAME = "scratch";
+    
     @Override
     public void onEnable() {
         // TODO Auto-generated method stub
@@ -67,9 +73,35 @@ public class UltimateDispenser extends JavaPlugin {
         this.hdConfig = new HDConfiguration(configFile);
     }
     
-    @Override
+ 
     public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args) {
         // TODO Auto-generated method stub
-        return false;
-    }
-}
+        System.out.println("Dispenser Set to Autofill");
+        // Parse out the command
+        String subcommand = args[0];
+        String[] subargs = new String[args.length - 1];
+        for(int i = 0; i < subargs.length; i++) {
+        subargs[i] = args[i + 1];
+          return false;      
+          
+       }
+        
+     // Check permission
+        if(!this.checkPermission(sender, PERMISSION_ROOT_NAME + "." + subcommand)) {
+        sender.sendMessage(ChatColor.RED + "You do not have the necessary permission to run /" + COMMAND_NAME + " " + subcommand);}
+        }
+
+	public boolean checkPermission(CommandSender sender1, String permission) {
+        	if(sender1 instanceof Player) {
+        	if(this.permissionHandler == null) {
+        	// No permissions; allow
+        	return true;
+        	} else {
+        	return this.permissionHandler.has((Player)sender1, permission);
+        	}
+        	} else {
+        	// Running from console; always allow
+        	return true;
+        	}
+        	}
+	}
