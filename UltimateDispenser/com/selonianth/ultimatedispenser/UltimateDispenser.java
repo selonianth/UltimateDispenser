e0de1f41bf62645cc401847d1d445f42d7834aec
Selonianth <lorienelf92@yahoo.com>
2011-08-05 22:57:20 -0400

    changed all swcongig and swconfigurations to hdconfig and hdconfiguration.

--- UltimateDispenser/com/selonianth/ultimatedispenser/UltimateDispenser.java
+++ UltimateDispenser/com/selonianth/ultimatedispenser/UltimateDispenser.java
@@ -11,7 +11,7 @@ import com.nijikokun.bukkit.Permissions.Permissions;
 
 public class UltimateDispenser extends JavaPlugin {
 
-    // public SWConfiguration swConfig;
+    public HDConfiguration hdConfig;
     public static final Logger LOG = Logger.getLogger("Minecraft");
     public static final String LOG_PREFIX = "[UltimateDispenser]";
     private PermissionHandler permissionHandler;
@@ -23,8 +23,8 @@ public class UltimateDispenser extends JavaPlugin {
 
     private void writeConfig() {
         // Save config
-        // if(!this.swConfig.write(this)) {
-        LOG.warning(LOG_PREFIX + "Failed to save configuration file (version " + this.getDescription().getVersion() + "); continuing anyway...");
+        if(!this.hdConfig.write(this)) {
+        LOG.warning(LOG_PREFIX + "Failed to save configuration file (version " + this.getDescription().getVersion() + "); continuing anyway...");}
     }
 
     @Override
@@ -39,14 +39,14 @@ public class UltimateDispenser extends JavaPlugin {
     private void setupPermissions() {
         Plugin permissionsPlugin = this.getServer().getPluginManager().getPlugin("Permissions");
 
-        // if(this.permissionHandler == null) {
+        if(this.permissionHandler == null) {
         if (permissionsPlugin != null) {
          this.permissionHandler = ((Permissions) permissionsPlugin).getHandler();
             LOG.info(LOG_PREFIX + "Hooked into Permissions version " + permissionsPlugin.getDescription().getVersion());
         } else {
             LOG.info(LOG_PREFIX + "Permissions system not detected; allowing all commands");
         }
-        // }
+        }
 
         // Get a configuration
         this.readConfig();
@@ -55,21 +55,20 @@ public class UltimateDispenser extends JavaPlugin {
     }
 
     private void readConfig() {
-        // Set up configuration folder
-        this.getDataFolder().mkdirs();
+    //Set up configuration folder
+     this.getDataFolder().mkdirs();
     }
 
-    // Read configuration file
-   // File configFile = new File(this.getDataFolder();{
-    // SWConfiguration.CONFIG_FILE_NAME);{
-    // this.swConfig = SWConfiguration.detectConfiguration(configFile);
-    //while(this.swConfig.getVersion() != SWConfiguration.LATEST_CONFIG_VERSION) {
-    //SWConfiguration upgraded = this.swConfig.upgrade();
-   // if(upgraded == null) {
-    //LOG.warning(LOG_PREFIX + "Unable to upgrade configuration from version "
-    // + this.swConfig.getVersion() + ".");
-    //LOG.warning(LOG_PREFIX +"You may experience some instability. Continuing anyway...");
-    //break;
-    //}
-    // this.swConfig = upgraded;}
-}
+    //Read configuration file
+    File configFile = new File(this.getDataFolder();{
+    HDConfiguration.CONFIG_FILE_NAME();
+    this.hdConfig = HDConfiguration.detectConfiguration(configFile);
+    while(this.hdConfig.getVersion() != HDConfiguration.LATEST_CONFIG_VERSION) {
+    HDConfiguration upgraded = this.hdConfig.upgrade();
+    if(upgraded == null) {
+    LOG.warning(LOG_PREFIX + "Unable to upgrade configuration from version "+ this.hdConfig.getVersion() + ".");
+    LOG.warning(LOG_PREFIX +"You may experience some instability. Continuing anyway...");
+    break;
+    }
+    this.hdConfig = upgraded;}
+}}
